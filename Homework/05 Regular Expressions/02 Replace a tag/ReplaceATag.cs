aed094b36ccda5774ccd1e9c9076e19921a9b9c3
vladaeca1996 <adaptatora@gmail.com>
2015-09-28 17:37:53 +0300

    Solve: Homework/05 Regular Expressions

---
+++ Homework/05 Regular Expressions/02 Replace a tag/ReplaceATag.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+class ReplaceATag
+{
+    static void Main()
+    {
+        string text = Console.ReadLine();
+        string pattern = @"<a.*href=((?:.|\n)*?(?=>))>((?:.|\n)*?(?=<))<\/a>";
+        string replace = @"[URL href=$1]$2[/URL]";
+        var replaced = Regex.Replace(text, pattern, replace);
+        Console.WriteLine(replaced);
+    }
+}
---
+++ Homework/05 Regular Expressions/03 Extract Emails/ExtractEmails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+class ExtractEmails
+{
+    static void Main()
+    {
+        string text = Console.ReadLine();
+        string patternMeil = @"(?<=\s|^)([a-z0-9]+(?:[_.-][a-z0-9]+)*@[a-z]+\-?[a-z]+(?:\.[a-z]+)+)\b";
+        Regex regexMail = new Regex(patternMeil);
+        MatchCollection matches = regexMail.Matches(text);
+        Console.WriteLine("Found {0} matches", matches.Count);
+        foreach (Match name in matches)
+        {
+            Console.WriteLine(name.Groups[0]);
+        }
+    }
+}
